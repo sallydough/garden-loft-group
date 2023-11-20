353d1e35e802ebaf0a6fd2ec000293c3c4bb06b7
Sally Do <56125850+sallydough@users.noreply.github.com>
2023-11-20 11:30:36 -0700

    edit containers

--- src/components/Contacts/Contact.jsx
+++ src/components/Contacts/Contact.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import './contact.css'
+
+import "./contact.css";
+
+
+
 
 const Contact = () => {
-  return (
-    <section id="contacts">Contacts</section>
-  )
-}
 
-export default Contact
+  return (
+    <section id="contacts">
+      <div className="contacts-main">
+        <article className="alphabet-button">
+          <h5>A - G</h5>
+        </article>
+        <article className="alphabet-button">
+          <h5>H - L</h5>
+        </article>
+        <article className="alphabet-button">
+          <h5>M - R</h5>
+        </article>
+        <article className="alphabet-button">
+          <h5>S - Z</h5>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
