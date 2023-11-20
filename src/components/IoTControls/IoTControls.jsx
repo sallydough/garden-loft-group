d73a3befcc13ffecfbc27cfd85896aa7bc845ce0
Sally Do <56125850+sallydough@users.noreply.github.com>
2023-11-20 12:14:06 -0700

    created map iot

--- src/components/IoTControls/IoTControls.jsx
+++ src/components/IoTControls/IoTControls.jsx
@@ -4,32 +4,60 @@ import LightbulbIcon from '@mui/icons-material/Lightbulb';
 import ThermostatIcon from '@mui/icons-material/Thermostat';
 import HttpsIcon from '@mui/icons-material/Https';
 import DevicesOtherIcon from '@mui/icons-material/DevicesOther';
-
+import { useState } from "react";
 
 
 const IoTControls = () => {
   const iconStyle = {
     fontSize:'70px'
   }
+
+  const [light, setLight] = useState("Light Info")
+
+const handleLightChange = () => {
+  const light = [ "Sally", "Lema", "Merseret", "Chitra", "Jocelyn"
+    
+  //   {
+  //   id: 1,
+  //   name: "Device 1",
+  //   state: true,
+  // },
+  // {
+  //   id: 2,
+  //   name: "Device 2",
+  //   state: true,
+  // }
+    
+  ];
+  const int = Math.floor(Math.random() * 5);
+  // setName will be pick a name from array above and push that name into current spot of "Josh"
+  setLight(light[int]);
+};
+
+
+
   return (
     <section id="iotControls">
       <div className="iotControls-main">
-        <article className="control__card">
+        <article onClick={handleLightChange}className="control__card">
           <LightbulbIcon style={iconStyle}/>
           <h5>Lights</h5>
         </article>
-        <article className="control__card">
+        <article onClick={handleLightChange} className="control__card">
         <ThermostatIcon style={iconStyle}/>
           <h5>Temperature</h5>
         </article>
-        <article className="control__card">
+        <article onClick={handleLightChange} className="control__card">
           <HttpsIcon style={iconStyle}/>
           <h5>Lock</h5>
         </article>
-        <article className="control__card">
+        <article onClick={handleLightChange}className="control__card">
           <DevicesOtherIcon style={iconStyle}/>
           <h5>Other</h5>
         </article>
+      </div>
+      <div className="light-content">
+        {light}
       </div>
     </section>
   );
