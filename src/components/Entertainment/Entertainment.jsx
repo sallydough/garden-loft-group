f2165633d57527aca95a89ed9fdd3762e969bf1f
Sally Do <56125850+sallydough@users.noreply.github.com>
2023-11-22 10:52:10 -0700

    edit iot

--- src/components/Entertainment/Entertainment.jsx
+++ src/components/Entertainment/Entertainment.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+// import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './entertainment.css'
 
 const Entertainment = () => {
+
+
+  const [lightReading, setLightReading] = useState(null);
+
+  useEffect(() => {
+      const socket = new WebSocket('ws://homeassistant.local:8123/api/websocket');  // Replace with the actual IP and port
+
+      socket.onmessage = (event) => {
+          const data = JSON.parse(event.data);
+          setLightReading(data.lightReading);
+      };
+
+      return () => {
+          socket.close();
+      };
+  }, []);
+
   return (
-    <section id="entertainment">Entertainment</section>
-  )
+      <div>
+          <h1>Light Sensor Reading: {lightReading}</h1>
+      </div>
+  );
 }
 
+
 export default Entertainment
+
