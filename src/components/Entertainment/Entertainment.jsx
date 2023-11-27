58f1cadf79ca8f8c15e6fa4c442c0648c7fdbc3c
Sally Do <56125850+sallydough@users.noreply.github.com>
2023-11-26 21:53:00 -0700

    edit flicking

--- src/components/Entertainment/Entertainment.jsx
+++ src/components/Entertainment/Entertainment.jsx
@@ -1,13 +1,30 @@
 import './entertainment.css'
+import Flicking from "@egjs/react-flicking";
+import "@egjs/react-flicking/dist/flicking.css";
 
 
 const Entertainment = () => {
-
-
   return (
-      <div id="entertainment">
-<p>Entertainment</p>
-      </div>
+<>
+<Flicking>
+        <div className="flicking-panel">1</div>
+        <div className="flicking-panel nested-wide">
+          <Flicking bounce="0" bound={true} nested={true}>
+            <div className="flicking-panel">2.1</div>
+            <div className="flicking-panel">2.2</div>
+            <div className="flicking-panel">2.3</div>
+          </Flicking>
+        </div>
+        <div className="flicking-panel nested-wide vertical">
+          <Flicking bounce="0" bound={true} horizontal={false}>
+            <div className="flicking-panel">3.1</div>
+            <div className="flicking-panel">3.2</div>
+            <div className="flicking-panel">3.3</div>
+          </Flicking>
+        </div>
+        <div className="flicking-panel">4</div>
+</Flicking>
+      </>
   );
 }
 
